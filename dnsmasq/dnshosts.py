80f17471e45a1b4d3d79c65761c8c778bebe3320
indigoparadox <indigo.repo@interfinitydynamics.info>
2018-09-02 12:59:21 -0400

    Reuse code for reservations.

--- dnsmasq/dnshosts.py
+++ dnsmasq/dnshosts.py
@@ -6,23 +6,17 @@ def combine_host_reservations( hosts, reservations ):
         lambda x: {
             # IP4 if present, otherwise IP6.
             'ip': x['ip4'] if 'ip4' in x else x['ip6'] if 'ip6' in x else None,
-            'mac': 
-                # Break out nested arrays with comprehension.
-                [r for s in
-
-                # Both MAC if present, otherwise just #1.
-                map( lambda y: 
-                        [y['mac1'], y['mac2']] if 'mac2' in y and 
-                            None != y['mac2'] 
-                        else [y['mac1']],
-
+            'mac':
                 # Grab reservation for this host.
-                filter( lambda z: 
-                    z['ip4'] == x['ip4'] if 'ip4' in x and 'ip4' in z else 
-                    z['ip6'] == x['ip6'] if 'ip6' in x and 'ip6' in z else
-                    None,
-                    reservations
-                ) ) for r in s],
+                [r for s in map(
+                    lambda y: y['mac'],
+                    filter( lambda z: 
+                        z['ip4'] == x['ip4'] if 'ip4' in x and 'ip4' in z else 
+                        z['ip6'] == x['ip6'] if 'ip6' in x and 'ip6' in z else
+                        None,
+                        reservations
+                    )
+                ) for r in s],
             'name': x['name']
         },
         hosts
@@ -37,13 +31,4 @@ def read_hosts( path='/etc/hosts' ):
             if host_match:
                 hosts += [host_match.groupdict()]
     return hosts
-
-def read_reservations( path='/etc/dnsmasq.conf' ):
-    reservations = []
-    with open( path, 'r' ) as config_file:
-        for line in config_file:
-            reserve_match = re_reserve.match( line )
-            if reserve_match:
-                reservations += [reserve_match.groupdict()]
-    return reservations
 
