b98dd052a81b344009aae76615a0c556b898c159
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2018-12-18 07:07:44 -0500

    More bulk work on config saving.

--- dnsmasq/dnsconfig.py
+++ dnsmasq/dnsconfig.py
@@ -3,18 +3,24 @@ import re
 
 DEFAULT_CONFIG_PATH='/etc/dnsmasq.conf'
 EMPTY_OPTIONS = ['expand-hosts', 'stop-dns-rebind', 'rebind-localhost-ok', 'dhcp-authoritative', 'enable-ra']
-RE_RESERVATION = re.compile( '(?P<disabled>#)?dhcp-host=(?P<mac>[0-9a-zA-Z:,]*),(?P<ip4>[0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*)' )
+RE_RESERVATION = re.compile( '(?P<disabled>#)?dhcp-host=(?P<mac>[0-9a-zA-Z:,]*),(?P<ip>[0-9:.]*)' )
 RE_SERVER = re.compile( '(?P<disabled>#)?server=\\/(?P<domain>[a-zA-Z0-9.\\-_]*)\\/(?P<address>[0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*)(#(?P<dest_port>[0-9]*))?(@(?P<interface>[a-zA-Z0-9.\\-_]*)(#(?P<src_port>[0-9]*))?)?' )
 RE_INTERFACE = re.compile( '(?P<disabled>#)?interface=(?P<interface>[a-zA-Z0-9]*)' )
 RE_CNAME = re.compile( '(?P<disabled>#)?cname=(?P<source>[a-zA-Z0-9.\\-_]*),(?P<dest>[a-zA-Z0-9.\\-_]*)' )
 RE_OPTIONS = re.compile( '(?P<disabled>#)?(?P<option>domain|resolv-file|dhcp-hostsfile|pid-file|expand-hosts|min-port|stop-dns-rebind|rebind-localhost-ok|dhcp-lease-max|dhcp-authoritative|enable-ra)(=(?P<value>.*))?' )
 
-def parse_lines( pattern, path=DEFAULT_CONFIG_PATH ):
-    res_list = []
+def parse_lines( pattern, path=DEFAULT_CONFIG_PATH, key=None ):
+    res_list = None
+    if key:
+        res_list = {}
+    else:
+        res_list = []
     with open( path, 'r' ) as config_file:
         for line in config_file:
             match = pattern.match( line )
-            if match:
+            if match and key:
+                res_list[match.group( key )] = match.groupdict()
+            elif match:
                 res_list += [match.groupdict()]
     return res_list
 
--- dnsmasq/dnshosts.py
+++ dnsmasq/dnshosts.py
@@ -5,25 +5,25 @@ def combine_host_reservations( hosts, reservations ):
     return map(
         lambda x: {
             # IP4 if present, otherwise IP6.
-            'ip': x['ip4'] if 'ip4' in x else x['ip6'] if 'ip6' in x else None,
+            'ip': x['ip'] if 'ip' in x else None,
             'mac':
                 # Grab reservation for this host.
                 [r for s in map(
                     lambda y: y['mac'],
                     filter( lambda z: 
-                        z['ip4'] == x['ip4'] if 'ip4' in x and 'ip4' in z else 
-                        z['ip6'] == x['ip6'] if 'ip6' in x and 'ip6' in z else
+                        z['ip'] == x['ip'] if 'ip' in x and 'ip' in z else 
                         None,
                         reservations
                     )
                 ) for r in s],
-            'name': x['name']
+            'name': x['name'],
+            'key': x['ip'].replace( '.', '' ) if 'ip' in x else None
         },
         hosts
     )
 
 def read_hosts( path='/etc/hosts' ):
-    re_host = re.compile( '((?P<ip4>[0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*)|(?P<ip6>[0-9a-fA-F:]*))\\s*(?P<name>[a-zA-Z0-9\\-_]*)' )
+    re_host = re.compile( '(?P<ip>[0-9:.]*)\\s*(?P<name>[a-zA-Z0-9\\-_]*)' )
     hosts = []
     with open( path, 'r' ) as hosts_file:
         for line in hosts_file:
